c1dd030f7359abb158da01882c883d862c5aa74d
夏より冬派 <>
2024-01-11 23:29:09 +0900

    feat: 環境構築＋firebaseセットアップまで

---
+++ app/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app'
+import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
+import { getApp } from 'firebase/app'
+
+initializeFirebaseApp()
+
+export default function App({ Component, pageProps }: AppProps) {
+  console.log(getApp())
+  return <Component {...pageProps} />
+}
