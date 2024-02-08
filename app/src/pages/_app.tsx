3b8fdc3d298692eb8c50c74f366e71fa8e62779b
夏より冬派 <>
2024-02-08 22:39:48 +0900

    feat: ヘッダー、ログアウト機能の追加

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -3,9 +3,17 @@ import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
 import { getApp } from 'firebase/app'
 import '../globals.css'
 
+import { AuthProvider } from '@src/feature/auth/provider/AuthProvider'
+import { Header } from '@src/component/Header/Header'
+
 initializeFirebaseApp()
 
 export default function App({ Component, pageProps }: AppProps) {
   console.log(getApp())
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Header />
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
